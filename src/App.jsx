3ef1e1369f8e04058ff1ce9152763856235a206b
Tushar <tushar.nyc20@gmail.com>
2025-10-03 23:12:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import axios from "axios";
+import { useEffect } from 'react';
+
+function DiceRollingApp() {
+  // useState hook to keep track of the dice roll value
+  const [diceValue, setDiceValue] = useState(1);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://official-joke-api.appspot.com/random_joke')
+      .then(response => setData(response.data))
+      .catch(error => console.error('There was an error!', error));
+  }, [])
+
+  const rollDice = () => {
+    const randomValue = Math.floor(Math.random() * 6) + 1;
+    setDiceValue(randomValue);
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1>Dice Rolling App  </h1>
+      <div style={styles.dice}>
+        <p style={styles.diceValue}>{diceValue}</p>
+      </div>
+      <h2>data</h2>
+      <button style={styles.button} onClick={rollDice}>
+        Roll Dice
+      </button>
+      {data ? <div>{JSON.stringify(data)}</div> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+// Inline styles for the component
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px',
+  },
+  dice: {
+    fontSize: '100px',
+    width: '150px',
+    height: '150px',
+    border: '5px solid purple',
+    borderRadius: '10px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '20px auto',
+    backgroundColor: '#f2f2f2',
+  },
+  diceValue: {
+    margin: '0',
+  },
+  button: {
+    padding: '10px 20px',
+    fontSize: '20px',
+    cursor: 'pointer',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+  },
+};
+
+export default DiceRollingApp;
